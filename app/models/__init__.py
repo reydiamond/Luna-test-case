2ef8b57b07c62bfc54d4f008d4162af5b3088f4f
Lukin Daniil <120090625+reydiamond@users.noreply.github.com>
2026-02-09 18:53:11 +0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,7 +2,6 @@ from .activity import Activity
 from .assotiations import organisation_activities
 from .building import Building
 from .organisation import Organisation, OrganisationPhone
-from assotiations import organisation_activities
 
 __all__ = [
     "Activity",
